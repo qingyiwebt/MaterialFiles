6fbddac5d7e9382d99c54c1be03b5d8c97f8417b
qingyiwebt <qingyiweb@gmail.com>
2024-10-05 19:27:12 +0800

    add: apply dpi to activities

--- app/src/main/java/me/zhanghai/android/files/app/AppActivity.kt
+++ app/src/main/java/me/zhanghai/android/files/app/AppActivity.kt
@@ -5,11 +5,14 @@
 
 package me.zhanghai.android.files.app
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import me.zhanghai.android.files.settings.Settings
 import me.zhanghai.android.files.theme.custom.CustomThemeHelper
 import me.zhanghai.android.files.theme.night.NightModeHelper
+import me.zhanghai.android.files.util.valueCompat
 
 abstract class AppActivity : AppCompatActivity() {
     private var isDelegateCreated = false
@@ -35,5 +38,14 @@ abstract class AppActivity : AppCompatActivity() {
             finish()
         }
         return true
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        val dpi = Settings.DPI.valueCompat
+        if (newBase != null && dpi != 0) {
+            val conf = newBase.resources.configuration
+            conf.densityDpi = dpi
+        }
+        super.attachBaseContext(newBase)
     }
 }
